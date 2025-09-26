a0a53bbb64dd4fa20f310d19b0a3186fd6b1cab8
Huxlyx <mtengler@gmail.com>
2025-09-27 01:44:44 +0200

    New data units for temperature and humidity

--- src/de/mechrain/protocol/DataUnitFactory.java
+++ src/de/mechrain/protocol/DataUnitFactory.java
@@ -12,14 +12,16 @@ import de.mechrain.protocol.AckDataUnit.AckBuilder;
 import de.mechrain.protocol.ConnectionDelayDataUnit.ConnectionDelayBuilder;
 import de.mechrain.protocol.DeviceSettingChangeDataUnit.DeviceSettingChangeBuilder;
 import de.mechrain.protocol.ErrorDataUnit.ErrorBuilder;
+import de.mechrain.protocol.HumidityDataUnit.HumidityBuilder;
 import de.mechrain.protocol.SoilMoistureAbsDataUnit.SoilMoistureAbsBuilder;
 import de.mechrain.protocol.SoilMoisturePercentDataUnit.SoilMoisturePercentBuilder;
 import de.mechrain.protocol.StatusMessageDataUnit.StatusMessageBuilder;
+import de.mechrain.protocol.TemperatureDataUnit.TemperatureBuilder;
 import de.mechrain.protocol.UdpBroadcastDelayDataUnit.UdpBroadcastDelayBuilder;
 import de.mechrain.util.Util;
 
 public class DataUnitFactory {
-	private static final Logger LOG = LogManager.getLogger(Logging.SERVER);
+	private static final Logger LOG = LogManager.getLogger(Logging.DATA);
 
 	public AbstractMechRainDataUnit getDataUnit(final byte[] header, final InputStream is) throws DataUnitValidationException, IOException {
 		final MRP mrp = MRP.fromByte(header[0]);
@@ -28,8 +30,7 @@ public class DataUnitFactory {
 		final byte[] payload;
 		payload = is.readNBytes(length);
 		
-		LOG.trace(() -> "Payload length " + length);
-		LOG.trace(() -> "Payload: " + Util.BYTES2HEX(payload, payload.length));
+		LOG.trace(() -> "Payload length " + length + " bytes: " + Util.BYTES2HEX(payload, payload.length));
 		
 		switch (mrp) {
 			case ACK:
@@ -62,10 +63,25 @@ public class DataUnitFactory {
 				return new SoilMoisturePercentBuilder()
 						.soilMoisturePercent(payload[0])
 						.build();
+			case TEMPERATURE:
+				final int tempBits = payload[3] << 24 
+					| (payload[2] & 0xFF) << 16 
+					| (payload[1] & 0xFF) << 8 
+					| (payload[0] & 0xFF);
+				return new TemperatureBuilder()
+						.temperature(Float.intBitsToFloat(tempBits))
+						.build();
+			case HUMIDITY:
+				final int humidBits = payload[3] << 24 
+				| (payload[2] & 0xFF) << 16 
+				| (payload[1] & 0xFF) << 8 
+				| (payload[0] & 0xFF);
+				return new HumidityBuilder()
+						.humidity(Float.intBitsToFloat(humidBits))
+						.build();
 			default:
-				LOG.warn("Unknown messsage type " + mrp.name());
+				LOG.warn("Unknown message type " + mrp.name());
 				return null;
 		}
-		
 	}
 }
